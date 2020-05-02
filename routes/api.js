22d6d85aef7efee24c76794202d3a698e089028d
Hannah Lee <leexhannah@gmail.com>
2020-05-02 14:49:22 -0700

    update transactions routes

--- routes/api.js
+++ routes/api.js
@@ -2,33 +2,39 @@ const express = require('express');
 const router = express.Router();
 const Transaction = require('../models/transaction');
 
-router.get('/transactions', function (req, res, next) {
-  Transaction.find((err, transactions) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(transactions);
-    }
+// all transcations
+router.get('/transactions', (req, res, next) => {
+  Transaction.find({}).then((transactions) => {
+    res.status(200).json({ success: true, data: transactions });
+  });
+});
+
+// find single transaction
+router.get('/transactions/:id', (req, res, next) => {
+  Transaction.findById({ _id: req.params.id }).then((transaction) => {
+    res.status(200).json({ success: true, data: transaction });
   });
 });
 
 router.post('/transactions', (req, res, next) => {
-  let transaction = new Transaction(req.body);
-  transaction
-    .save()
-    .then((transaction) => {
-      // res.status(200).json({ transaction: 'transaction added successfully' });
-      res.send(transaction);
-    })
-    .catch(next);
+  const transaction = new Transaction(req.body);
+  transaction.save().then((transaction) => {
+    res.status(200).json({ success: true, data: transaction });
+  });
 });
 
-router.put('/transactions/:id', function (req, res, next) {
-  res.send({ type: 'PUT' });
+router.put('/transactions/:id', (req, res, next) => {
+  Transaction.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
+    Transaction.findOne({ _id: req.params.id }).then((transaction) => {
+      res.status(200).json({ success: true, data: transaction });
+    });
+  });
 });
 
-router.delete('/transactions/:id', function (req, res, next) {
-  res.send({ type: 'DELETE' });
+router.delete('/transactions/:id', (req, res, next) => {
+  Transaction.findByIdAndDelete({ _id: req.params.id }).then((transaction) => {
+    res.status(200).json({ success: true, data: transaction });
+  });
 });
 
 module.exports = router;
