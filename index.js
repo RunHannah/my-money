1a2813447ea35f49577cc2001fcf0f53c5840445
Hannah Lee <leexhannah@gmail.com>
2020-05-02 16:03:28 -0700

    delete data when connecting to database

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Transaction = require('./models/transaction');
 
 const uri = require('./config/keys').mongoURI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
-// db.once('open', (_) => {
-//   console.log('Database connected:', db);
-// });
+db.once('open', async (_) => {
+  // console.log('Database connected:', db);
+  const eraseDatabaseOnSync = true;
+  if (eraseDatabaseOnSync) {
+    await Promise.all([Transaction.deleteMany({})]);
+  }
+});
 
 // db.on('error', (err) => {
 //   console.error('connection error:', err);
