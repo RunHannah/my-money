3c7c01f1777f301265ea79e469bc0244623b8d70
Hannah Lee <leexhannah@gmail.com>
2020-05-03 21:45:50 -0700

    check for duplicate emails

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,11 @@ router.post('/register', async (req, res, next) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // check duplicate email
+  const checkDupEmail = await User.findOne({ email: req.body.email });
+  if (checkDupEmail)
+    return res.status(400).send('This email has already been registered.');
+
   const user = new User(req.body);
   await user.save().then((user) => {
     res.status(200).json({ success: true, data: user });
