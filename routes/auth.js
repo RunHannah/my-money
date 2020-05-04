19d4905b417e2a8e055778a6cda7d8c54965ab68
Hannah Lee <leexhannah@gmail.com>
2020-05-03 20:39:12 -0700

    create User model and add to database

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.post('/register', (req, res, next) => {
-  res.send('Register');
+router.post('/register', async (req, res, next) => {
+  const user = new User(req.body);
+  await user.save().then((user) => {
+    res.status(200).json({ success: true, data: user });
+  });
 });
 
 module.exports = router;
