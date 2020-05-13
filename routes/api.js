6b58a41fe4cc40cad4749235893716a30c1d6f70
Hannah Lee <leexhannah@gmail.com>
2020-05-12 22:18:11 -0700

    update api routes to use async

--- routes/api.js
+++ routes/api.js
@@ -4,45 +4,45 @@ const Transaction = require('../models/transaction');
 const verify = require('../middleware/verifyToken');
 
 // all transactions
-router.get('/transactions', (req, res, next) => {
-  Transaction.find({}).then((transactions) => {
-    res.status(200).json({ success: true, data: transactions });
-  });
+router.get('/transactions', async (req, res, next) => {
+  const transactions = await Transaction.find({});
+  res.status(200).json({ success: true, data: transactions });
 });
 
 // find single transaction
-router.get('/transactions/:id', (req, res, next) => {
-  Transaction.findById({ _id: req.params.id }).then((transaction) => {
-    res.status(200).json({ success: true, data: transaction });
-  });
+router.get('/transactions/:id', async (req, res, next) => {
+  const transaction = await Transaction.findById({ _id: req.params.id });
+  res.status(200).json({ success: true, data: transaction });
 });
 
 // find all transactions by user
-router.get('/transactions/user/:userId', verify, (req, res, next) => {
-  Transaction.find({ userId: req.params.userId }).then((transactions) => {
-    res.status(200).json({ success: true, data: transactions });
-  });
+router.get('/transactions/user/:userId', verify, async (req, res, next) => {
+  const transactions = await Transaction.find({ userId: req.params.userId });
+  res.status(200).json({ success: true, data: transactions });
 });
 
-router.post('/transactions', verify, (req, res, next) => {
+router.post('/transactions', verify, async (req, res, next) => {
   const transaction = new Transaction(req.body);
-  transaction.save().then((transaction) => {
-    res.status(200).json({ success: true, data: transaction });
-  });
+  const savedTransaction = await transaction.save();
+  res.status(200).json({ success: true, data: savedTransaction });
 });
 
-router.put('/transactions/:id', verify, (req, res, next) => {
-  Transaction.findByIdAndUpdate({ _id: req.params.id }, req.body).then(() => {
-    Transaction.findOne({ _id: req.params.id }).then((transaction) => {
-      res.status(200).json({ success: true, data: transaction });
-    });
-  });
+router.put('/transactions/:id', verify, async (req, res, next) => {
+  const updated = await Transaction.findByIdAndUpdate(
+    { _id: req.params.id },
+    req.body
+  );
+  if (updated) {
+    const transaction = await Transaction.findOne({ _id: req.params.id });
+    res.status(200).json({ success: true, data: transaction });
+  }
 });
 
-router.delete('/transactions/:id', verify, (req, res, next) => {
-  Transaction.findByIdAndDelete({ _id: req.params.id }).then((transaction) => {
-    res.status(200).json({ success: true, data: transaction });
+router.delete('/transactions/:id', verify, async (req, res, next) => {
+  const transaction = await Transaction.findByIdAndDelete({
+    _id: req.params.id,
   });
+  res.status(200).json({ success: true, data: transaction });
 });
 
 module.exports = router;
