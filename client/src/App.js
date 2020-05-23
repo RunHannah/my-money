899020392607edae3cc433757e0f0c4d4dcaf1a5
Hannah Lee <leexhannah@gmail.com>
2020-05-22 21:31:30 -0700

    request data from client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return <div className='App'>Finance Tracker</div>;
+  const [data, setData] = useState(null);
+  console.log('data', data);
+  useEffect(() => {
+    const getTransactions = async () => {
+      const result = await axios('/api/transactions');
+
+      setData(result.data);
+    };
+    getTransactions();
+  }, []);
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Personal Finance Tracker</p>
+      </header>
+    </div>
+  );
 }
 
 export default App;
