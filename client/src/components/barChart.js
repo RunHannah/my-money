82dabb54c8b692d92f5a8ce267576a090c6480e1
Hannah Lee <leexhannah@gmail.com>
2020-05-24 14:13:24 -0700

    move getTransactions from useEffect and add css class

--- client/src/components/barChart.js
+++ client/src/components/barChart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Bar } from 'react-chartjs-2';
 import axios from 'axios';
+import './barChart.css';
 
 const BarChart = () => {
   const [data, setData] = useState({});
@@ -20,71 +21,76 @@ const BarChart = () => {
     'Dec',
   ];
 
-  let totalMonth = {
-    Jan: 0,
-    Feb: 0,
-    Mar: 0,
-    Apr: 0,
-    May: 0,
-    Jun: 0,
-    Jul: 0,
-    Aug: 0,
-    Sep: 0,
-    Oct: 0,
-    Nov: 0,
-    Dec: 0,
-  };
+  function changeToDollar(value) {
+    return '$' + Number(value.toFixed(1)).toLocaleString();
+  }
 
-  useEffect(() => {
-    async function getTransactions() {
-      await axios
-        .get('/api/transactions')
-        .then((res) => {
-          for (const dataObj of res.data.data) {
-            let month = months[new Date(dataObj.date).getMonth()];
-            let amount = parseInt(dataObj.amount);
+  async function getTransactions() {
+    let totalMonth = {
+      Jan: 0,
+      Feb: 0,
+      Mar: 0,
+      Apr: 0,
+      May: 0,
+      Jun: 0,
+      Jul: 0,
+      Aug: 0,
+      Sep: 0,
+      Oct: 0,
+      Nov: 0,
+      Dec: 0,
+    };
 
-            if (month in totalMonth) {
-              totalMonth[month] += amount;
-            }
+    await axios
+      .get('/api/transactions')
+      .then((res) => {
+        for (const dataObj of res.data.data) {
+          let month = months[new Date(dataObj.date).getMonth()];
+          let amount = parseInt(dataObj.amount);
+
+          if (month in totalMonth) {
+            totalMonth[month] += amount;
           }
+        }
 
-          setData({
-            labels: Object.keys(totalMonth),
-            datasets: [
-              {
-                label: 'transactions',
-                data: Object.values(totalMonth),
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: 'rgb(255, 99, 132)',
-                borderWidth: 2,
-              },
-            ],
-          });
-        })
-        .catch((err) => {
-          console.log(err);
+        console.log('totalMonth', totalMonth);
+        setData({
+          labels: Object.keys(totalMonth),
+          datasets: [
+            {
+              label: 'transactions',
+              data: Object.values(totalMonth),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: 'rgb(255, 99, 132)',
+              borderWidth: 2,
+            },
+          ],
         });
-    }
-
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => {
     getTransactions();
-  }, [data]);
+    console.log('data', data);
+  }, []);
 
   return (
-    <div className='chart'>
+    <div className='barChart'>
       <h1>Bar Chart</h1>
       <div>
         <Bar
@@ -99,6 +105,9 @@ const BarChart = () => {
                     autoSkip: true,
                     maxTicksLimit: 10,
                     beginAtZero: true,
+                    callback: function (value, index, values) {
+                      return changeToDollar(value);
+                    },
                   },
                   gridLines: {
                     display: true,
