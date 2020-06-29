65008c17385c89d50d9acd9ff82c82d4e3b1df1a
Hannah Lee <leexhannah@gmail.com>
2020-06-28 22:35:55 -0700

    sideNav for mobile

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,43 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { UserContext } from '../../contexts/userContext';
 import './navbar.css';
 
 const NavBar = () => {
   const { user } = useContext(UserContext);
+  const [navStatus, setNavStatus] = useState('closed');
+  const sideNav = window.innerWidth < 768 ? 'sideNav' : 'desktopNav';
+
+  const openNav = () => setNavStatus('open');
+  const closeNav = () => setNavStatus('closed');
 
   return (
-    <nav className='navbar'>
-      <h1 className='navName'>Personal Finance Tracker</h1>
-      <div className='navItems'>
-        <NavLink className='navCharts' to='/charts'>
-          Charts
-        </NavLink>
-        {!user && (
-          <React.Fragment>
-            <NavLink className='navLogin' to='/login'>
-              Login
-            </NavLink>
-            <NavLink className='navRegister' to='/register'>
-              Register
-            </NavLink>
-          </React.Fragment>
-        )}
-        {user && (
-          <React.Fragment>
-            <NavLink className='navProfile' to='/transactions'>
-              Transactions
-            </NavLink>
-            <NavLink className='navProfile' to='/profile'>
-              {user.name}
-            </NavLink>
-            <NavLink className='navLogout' to='/logout'>
-              Logout
-            </NavLink>
-          </React.Fragment>
-        )}
+    <div className={`navbar ${sideNav}`}>
+      <div className='navMenu'>
+        <span className='openBtn' onClick={openNav}>
+          &#9776;
+        </span>
+        <h1 className='navName'>Personal Finance Tracker</h1>
       </div>
-    </nav>
+      <nav className={sideNav === 'sideNav' ? navStatus : 'fullNav'}>
+        <span className='closeBtn' onClick={closeNav}>
+          &times;
+        </span>
+        <div className='navItems'>
+          <NavLink className='navCharts' to='/charts'>
+            Charts
+          </NavLink>
+          {!user && (
+            <React.Fragment>
+              <NavLink className='navLogin' to='/login'>
+                Login
+              </NavLink>
+              <NavLink className='navRegister' to='/register'>
+                Register
+              </NavLink>
+            </React.Fragment>
+          )}
+          {user && (
+            <React.Fragment>
+              <NavLink className='navProfile' to='/transactions'>
+                Transactions
+              </NavLink>
+              <NavLink className='navProfile' to='/profile'>
+                {user.name}
+              </NavLink>
+              <NavLink className='navLogout' to='/logout'>
+                Logout
+              </NavLink>
+            </React.Fragment>
+          )}
+        </div>
+      </nav>
+    </div>
   );
 };
 
