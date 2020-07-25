ca707c84f27336003674825d0571cf2769eb8e38
Hannah Lee <leexhannah@gmail.com>
2020-07-25 15:47:15 -0700

    return errors for invvalid csv file

--- client/src/components/upload/upload.js
+++ client/src/components/upload/upload.js
@@ -1,6 +1,5 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import { parse } from 'papaparse';
-import { DataContext } from '../../contexts/dataContext';
 import { UserContext } from '../../contexts/userContext';
 import transact from '../../services/transactService';
 import './upload.css';
@@ -8,93 +7,81 @@ import './upload.css';
 function Upload() {
   const [highlighted, setHighlighted] = useState(false);
   const [csvData, setCsvData] = useState([]);
+  const [errorsList, setErrorsList] = useState([]);
   const { user } = useContext(UserContext);
-  const { setData } = useContext(DataContext);
 
-  async function getUserTransactions(userId) {
-    try {
-      const res = await transact.getUserTransactions(userId);
-      setData(res.data.data);
-    } catch (err) {
-      console.log('getUserTransactions', err);
-    }
-  }
+  const checkCsvData = useCallback(() => {
+    if (csvData.length > 0) {
+      let errors = [];
+      csvData.map(async (item) => {
+        let record = {};
+
+        Object.keys(item).map((key) => {
+          key = key.toLowerCase();
+
+          switch (key) {
+            case 'category':
+              {
+                let val = item[key].trim().toLowerCase();
+                if (
+                  ![
+                    'food',
+                    'entertainment',
+                    'health',
+                    'other',
+                    'auto',
+                    'travel',
+                    'home',
+                  ].includes(val)
+                ) {
+                  val = 'other';
+                }
+                const valUpdated = val[0].toUpperCase() + val.slice(1);
+                record[`${key}`] = valUpdated;
+              }
+              break;
+            case 'amount':
+              {
+                const val = item[key].trim();
+                if (!/^[0-9]*$/gm.test(val)) {
+                  errors.push(`${val} is not valid number`);
+                } else {
+                  record[`${key}`] = val;
+                }
+              }
+              break;
+            case 'description':
+              {
+                const val = item[key].trim();
+                record[`${key}`] = val;
+              }
+              break;
+            case 'date':
+              {
+                const val = item[key].trim();
+                record[`${key}`] = val;
+              }
+              break;
+            default:
+              errors.push(`${key} is an invalid field`);
+              break;
+          } // end of switch case
+          return record;
+        }); // end of keys loop
+
+        setErrorsList([...new Set(errors)]);
+        if (errors.length === 0) {
+          record.userId = user.id;
+          await transact.addNewTransaction(record);
+        }
+        errors = [];
+      });
+    } // end of csv loop
+  }, [csvData, user.id]); // end of function
 
   useEffect(() => {
-    const checkCsvData = () => {
-      if (csvData.length > 0) {
-        let errors = [];
-        csvData.map(async (item) => {
-          let record = {};
-
-          Object.keys(item).map((key) => {
-            key = key.toLowerCase();
-
-            switch (key) {
-              case 'category':
-                {
-                  let val = item[key].trim().toLowerCase();
-                  if (
-                    ![
-                      'food',
-                      'entertainment',
-                      'health',
-                      'other',
-                      'auto',
-                      'travel',
-                      'home',
-                    ].includes(val)
-                  ) {
-                    val = 'other';
-                  }
-                  const valUpdated = val[0].toUpperCase() + val.slice(1);
-                  record[`${key}`] = valUpdated;
-                }
-                break;
-              case 'amount':
-                {
-                  const val = item[key].trim();
-                  if (!/^[0-9]*$/gm.test(val)) {
-                    errors.push(`${val} is not valid number`);
-                  } else {
-                    record[`${key}`] = val;
-                  }
-                }
-                break;
-              case 'description':
-                {
-                  const val = item[key].trim();
-                  record[`${key}`] = val;
-                }
-                break;
-              case 'date':
-                {
-                  const val = item[key].trim();
-                  record[`${key}`] = val;
-                }
-                break;
-              default:
-                errors.push(`${key} is an invalid field}`);
-                break;
-            } // end of switch case
-            return record;
-          }); // end of item loop
-          if (errors.length === 0) {
-            record.userId = user.id;
-            await transact.addNewTransaction(record);
-          }
-        });
-        if (errors.length > 0) {
-          console.log('errors', errors);
-        }
-      } // end of if condition
-      getUserTransactions(user.id);
-    }; // end of function
-
-    if (csvData.length > 0) {
-      checkCsvData();
-    }
-  }, [csvData, user.id]); // end of useEffect
+    checkCsvData();
+  }, [checkCsvData]);
 
   return (
     <div className='uploadWrapper'>
@@ -124,12 +111,20 @@ function Upload() {
       >
         Drag and drop .CSV file here
       </div>
-      <>
-        <p className='uploadText'>
-          Valid CSV files contain only the following columns: Date, Category,
-          Description, and Amount
-        </p>
-      </>
+      <div className='uploadText'>
+        {errorsList.length === 0 ? (
+          <p>
+            Valid CSV files contain only the following columns: Date, Category,
+            Description, and Amount
+          </p>
+        ) : (
+          <ul>
+            {errorsList.map((error, index) => (
+              <li key={index}>{error}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
