ddf48aef1fc10ea646431cfe3efd9e497d474598
Hannah Lee <leexhannah@gmail.com>
2020-07-26 22:38:11 -0700

    different device styling for bar charts

--- client/src/components/charts/barChart.js
+++ client/src/components/charts/barChart.js
@@ -2,20 +2,16 @@ import React, { useState, useEffect, useContext, useCallback } from 'react';
 import { DataContext } from '../../contexts/dataContext';
 import { Bar } from 'react-chartjs-2';
 import getTotalCategory from '../../utils/getTotalCategory';
+import barStyling from './styling/barStyling';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import './barChart.css';
 
 const BarChart = () => {
-  const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
   const { data } = useContext(DataContext);
   const [barMonthData, setBarMonthData] = useState({});
   const [barCategoryData, setBarCategoryData] = useState({});
-
-  function changeToDollar(value) {
-    return '$' + Number(value.toFixed(1)).toLocaleString();
-  }
 
   const loadMonthData = useCallback(() => {
     const months = [
@@ -109,126 +105,6 @@ const BarChart = () => {
     });
   }, [data]);
 
-  const barDesktopStyle = {
-    responsive: true,
-    title: {
-      text: '$ Total Spending',
-      display: true,
-      fontSize: 15,
-      padding: 20,
-    },
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            autoSkip: true,
-            maxTicksLimit: 10,
-            beginAtZero: true,
-            callback: function (value, index, values) {
-              return changeToDollar(value);
-            },
-            fontSize: 12,
-          },
-          gridLines: {
-            display: true,
-          },
-        },
-      ],
-      xAxes: [
-        {
-          gridLines: {
-            display: false,
-          },
-        },
-      ],
-    },
-    layout: {
-      padding: {
-        left: 10,
-        right: 10,
-        top: 0,
-        bottom: 10,
-      },
-    },
-    legend: {
-      display: false,
-    },
-    plugins: {
-      datalabels: {
-        color: '#000',
-        anchor: 'end',
-        align: 'end',
-        offset: -5,
-        font: {
-          size: 14,
-        },
-        formatter: function (value) {
-          return '$' + value;
-        },
-      },
-    },
-  };
-
-  const barMobileStyle = {
-    responsive: true,
-    title: {
-      text: '$ Total Spending',
-      display: true,
-      fontSize: 12,
-      padding: 12,
-    },
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            autoSkip: true,
-            maxTicksLimit: 5,
-            beginAtZero: true,
-            callback: function (value) {
-              return changeToDollar(value);
-            },
-            fontSize: 10,
-          },
-          gridLines: {
-            display: true,
-          },
-        },
-      ],
-      xAxes: [
-        {
-          gridLines: {
-            display: false,
-          },
-        },
-      ],
-    },
-    layout: {
-      padding: {
-        left: 5,
-        right: 5,
-        top: 0,
-        bottom: 5,
-      },
-    },
-    legend: {
-      display: false,
-    },
-    plugins: {
-      datalabels: {
-        color: '#000',
-        anchor: 'end',
-        align: 'end',
-        offset: -5,
-        font: {
-          size: 10,
-        },
-        formatter: function (value) {
-          return '$' + value;
-        },
-      },
-    },
-  };
-
   useEffect(() => {
     loadMonthData();
   }, [loadMonthData]);
@@ -239,11 +115,9 @@ const BarChart = () => {
 
   useEffect(() => {
     if (window.innerWidth > 769) {
-      setIsDesktop(true);
       setIsMobile(false);
     } else {
       setIsMobile(true);
-      setIsDesktop(false);
     }
   }, []);
 
@@ -251,11 +125,11 @@ const BarChart = () => {
     <div className='barCharts'>
       <Bar
         data={barMonthData}
-        options={isMobile ? barMobileStyle : barDesktopStyle}
+        options={isMobile ? barStyling.mobileYear : barStyling.desktopYear}
       />
       <Bar
         data={barCategoryData}
-        options={isMobile ? barMobileStyle : barDesktopStyle}
+        options={isMobile ? barStyling.mobileCategory : barStyling.desktopYear}
       />
     </div>
   );
---
+++ client/src/components/charts/styling/barStyling.js
@@ -0,0 +1,240 @@
+const barStyling = {
+  mobileYear: {
+    responsive: true,
+    title: {
+      text: '$ Total Spending for Year',
+      display: true,
+      fontSize: 12,
+      padding: 12,
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 5,
+            beginAtZero: true,
+            callback: function (value) {
+              return '$' + Number(value.toFixed(1)).toLocaleString();
+            },
+            fontSize: 10,
+          },
+          gridLines: {
+            display: true,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+        },
+      ],
+    },
+    layout: {
+      padding: {
+        left: 5,
+        right: 5,
+        top: 0,
+        bottom: 5,
+      },
+    },
+    legend: {
+      display: false,
+    },
+    plugins: {
+      datalabels: {
+        color: '#000',
+        anchor: 'end',
+        align: 'end',
+        offset: -5,
+        font: {
+          size: 10,
+        },
+        formatter: function (value) {
+          return '$' + value;
+        },
+      },
+    },
+  },
+  desktopYear: {
+    responsive: true,
+    title: {
+      text: '$ Total Spending for Year',
+      display: true,
+      fontSize: 15,
+      padding: 20,
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 10,
+            beginAtZero: true,
+            callback: function (value) {
+              return '$' + Number(value.toFixed(1)).toLocaleString();
+            },
+            fontSize: 12,
+          },
+          gridLines: {
+            display: true,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+        },
+      ],
+    },
+    layout: {
+      padding: {
+        left: 10,
+        right: 10,
+        top: 0,
+        bottom: 10,
+      },
+    },
+    legend: {
+      display: false,
+    },
+    plugins: {
+      datalabels: {
+        color: '#000',
+        anchor: 'end',
+        align: 'end',
+        offset: -5,
+        font: {
+          size: 14,
+        },
+        formatter: function (value) {
+          return '$' + value;
+        },
+      },
+    },
+  },
+  mobileCategory: {
+    responsive: true,
+    title: {
+      text: '$ Total Spending by Category',
+      display: true,
+      fontSize: 12,
+      padding: 12,
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 5,
+            beginAtZero: true,
+            callback: function (value) {
+              return '$' + Number(value.toFixed(1)).toLocaleString();
+            },
+            fontSize: 10,
+          },
+          gridLines: {
+            display: true,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+        },
+      ],
+    },
+    layout: {
+      padding: {
+        left: 5,
+        right: 5,
+        top: 0,
+        bottom: 5,
+      },
+    },
+    legend: {
+      display: false,
+    },
+    plugins: {
+      datalabels: {
+        color: '#000',
+        anchor: 'end',
+        align: 'end',
+        offset: -5,
+        font: {
+          size: 10,
+        },
+        formatter: function (value) {
+          return '$' + value;
+        },
+      },
+    },
+  },
+  desktopCategory: {
+    responsive: true,
+    title: {
+      text: '$ Total Spending by Category',
+      display: true,
+      fontSize: 15,
+      padding: 20,
+    },
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            autoSkip: true,
+            maxTicksLimit: 10,
+            beginAtZero: true,
+            callback: function (value) {
+              return '$' + Number(value.toFixed(1)).toLocaleString();
+            },
+            fontSize: 12,
+          },
+          gridLines: {
+            display: true,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+        },
+      ],
+    },
+    layout: {
+      padding: {
+        left: 10,
+        right: 10,
+        top: 0,
+        bottom: 10,
+      },
+    },
+    legend: {
+      display: false,
+    },
+    plugins: {
+      datalabels: {
+        color: '#000',
+        anchor: 'end',
+        align: 'end',
+        offset: -5,
+        font: {
+          size: 14,
+        },
+        formatter: function (value) {
+          return '$' + value;
+        },
+      },
+    },
+  },
+};
+
+export default barStyling;
